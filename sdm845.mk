0b719b4c8b1170f1400d979379d2bad23630c311
Demon000 <demonsingur@gmail.com>
2019-06-08 10:51:49 +0200

    Revert "sdm845-common: Update vndk-sp build mechanism and library set."
    This reverts commit 65b1704a3234b7e4cc9547da4a4b4aedc14a2d02.

--- sdm845.mk
+++ sdm845.mk
@@ -98,6 +98,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.trust@1.0-service
 
-# VNDK-SP
 PRODUCT_PACKAGES += \
-    vndk-sp
+    android.hardware.renderscript@1.0.vndk-sp \
+    android.hardware.graphics.allocator@2.0.vndk-sp \
+    android.hardware.graphics.mapper@2.0.vndk-sp \
+    android.hardware.graphics.common@1.0.vndk-sp \
+    android.hidl.base@1.0.vndk-sp \
+    libhwbinder.vndk-sp \
+    libbase.vndk-sp \
+    libcutils.vndk-sp \
+    libhardware.vndk-sp \
+    libhidlbase.vndk-sp \
+    libhidltransport.vndk-sp \
+    libutils.vndk-sp \
+    libc++.vndk-sp \
+    libsync.vndk-sp \
+    libbacktrace.vndk-sp \
+    libunwind.vndk-sp \
+    liblzma.vndk-sp
